67efc133b16762e11c62875b089c6cad974ada70
Jonatas Fischer <jonatas.fischer@hotmail.com>
2022-07-29 13:07:29 +0200

    code clean up

--- cypress/support/page_objects/admin/product/Categories.js
+++ cypress/support/page_objects/admin/product/Categories.js
@@ -1,6 +1,4 @@
 const CATEGORY_LINK = (name) => `td.categories_view_data > b > a:contains("${name}")`;
-//div[data-config_value="BUTTON_PROPERTIES"]
-const PRODUCT_LINK = (name) => `td.categories_view_data > b > a:contains("${title}")`;
 const PRODUCT_ROW = (name) => `tr.dataTableRow > td > a:contains(${name})`;
 const PRODUCT_ROW_UNIQUE = (name) => `tr.dataTableRow > td > a:contains(${name}):first`;
 const PRODUCT_ACTIONS_BUTTON = () => `button > i.fa-caret-down`;
@@ -9,10 +7,6 @@ const PRODUCT_ATTRIBUTES_LINK = () => `span[data-value="BUTTON_EDIT_ATTRIBUTES"]
 const PRODUCT_EDIT_LINK = () => `span[data-value="edit"]`;
 
 export default class Categories {
-
-    static editPropertiesForProduct(product) {
-
-    }
 
     static visit() {
         cy.visit('/admin/categories.php?language=en')
@@ -35,22 +29,16 @@ export default class Categories {
         cy.get(PRODUCT_ROW(productName))
             .parents('tr:first')
             .find(PRODUCT_ACTIONS_BUTTON())
-            .click({force: true}).parents('div.product-dropdown-button:first').within(
-            ($button) => {
-                cy.get(PRODUCT_OPTIONS_LINK()).click()
-            }
-        );
+            .click({force: true}).parents('div.product-dropdown-button:first')
+            .within(() => cy.get(PRODUCT_OPTIONS_LINK()).click());
     }
 
     static navigateProductAttributes(productName) {
         cy.get(PRODUCT_ROW(productName))
             .parents('tr:first')
             .find(PRODUCT_ACTIONS_BUTTON())
-            .click({force: true}).parents('div.product-dropdown-button:first').within(
-            ($button) => {
-                cy.get(PRODUCT_ATTRIBUTES_LINK()).click()
-            }
-        );
+            .click({force: true}).parents('div.product-dropdown-button:first')
+            .within(() => cy.get(PRODUCT_ATTRIBUTES_LINK()).click());
     }
 
 
@@ -58,11 +46,8 @@ export default class Categories {
         cy.get(PRODUCT_ROW(productName))
             .parents('tr:first')
             .find(PRODUCT_ACTIONS_BUTTON())
-            .click({force: true}).parents('div.product-dropdown-button:first').within(
-            ($button) => {
-                cy.get(PRODUCT_EDIT_LINK()).click()
-            }
-        );
+            .click({force: true}).parents('div.product-dropdown-button:first')
+            .within(() => cy.get(PRODUCT_EDIT_LINK()).click());
     }
 
     static clickCreateProduct() {
@@ -77,20 +62,17 @@ export default class Categories {
         cy.get('body').then(($body) => {
             if ($body.find(PRODUCT_ROW_UNIQUE(productName)).length > 0) {   //evaluates as true
                 cy.get(PRODUCT_ROW_UNIQUE(productName)).parents('tr.dataTableRow')
-                    .find('span.single-checkbox').scrollIntoView().click()
-                    .then((elements) => {
+                    .find('span.single-checkbox').scrollIntoView()
+                    .click()
+                    .then(() => {
                         cy.get('div[data-config_key="multiCategoryOverviewDropdownBtn"] > ul > li > span[data-value="delete"]').click({force: true})
                             .then(() => {
                                 cy.waitDefaultAdminRequests()
                                     .get('div.ui-dialog-buttonset > button.btn-primary:contains("Delete")').click()
                                     .waitDefaultAdminRequests()
-                                    .wait(3000)
-                                    .then((obj) => {
-                                        Categories.deleteProductsNamedAs(productName)
-                                    })
-                                //this.deleteProductsNamedAs(productName)
+                                    .wait(100)
+                                    .then(() => Categories.deleteProductsNamedAs(productName))
                             })
-
                     })
             }
         })
